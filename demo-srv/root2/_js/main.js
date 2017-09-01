e34a4b6258cdaa53f7b1577b4be4fa4475dc0f87
Wolfgang <wgehner@gmail.com>
2017-09-01 05:14:46 -0700

    Add Std CustomElement

--- demo-srv/root2/_js/main.js
+++ demo-srv/root2/_js/main.js
@@ -9,6 +9,7 @@ var TM = {
 			TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/vendor/jquery.jsForm.min.js')
 			, TS.load('//cdn.jsdelivr.net/dot.js/1.1.1/doT.min.js')
 			//, TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/webComps/tw0-1.0.js').then(function(){TW.init()}) //Support for Standard Web Component
+			, TS.load('/_js/tw2-1.0.js') //Late-loader Helper for Standard Web components
 			, TS.load('/_js/tp2-1.0.js') //Late-loader Helper for Polymer 2 components
 			, TS.load('/bower_components/webcomponentsjs/webcomponents-lite.js') //Polymer Polyfills
 			, TS.load('/bower_components/polymer/polymer.html') //Support for Polymer (2.0.0)
