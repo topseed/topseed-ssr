b1e13dbd5e3b6c595eaa4b6cfed373c2640fcfb5
Wolfgang <wgehner@gmail.com>
2017-09-01 15:21:18 -0700

    Cleanup

--- ssr-topseed-io/server/route/dashboard/index.js
+++ ssr-topseed-io/server/route/dashboard/index.js
@@ -9,40 +9,41 @@ const doT = require('dot')
 require('@skatejs/ssr/register')
 const render = require('@skatejs/ssr')
 
-const myssrcomp = require("../../ssrComp/Myssrcomp")
-
 const Util = require('topseed-utils')
 const U = new Util()
 const ROOT = './' + ServerConfig.WEBROOT
 
 const requestedResource = ROOT + '/page/dashboard/index.pug'
 
+//component class
+const Myssrcomp = require("../../ssrComp/Myssrcomp")
+
 router.get('/', function (req, res) {
 	
 		console.log('dashboard SSR')
 		const $page = U.getAsDoc(requestedResource)
-		var html = $page.html()
-		console.log
 
+		//here we only look for one ssr component, but we could loop through all
+
+		//component dom, for use by component class
 		global.$ = U.getAsDoc(ROOT + '/_webComp/Myssrcomp.pug')
-		var $tpl = $('template')
-		//$comp('style').remove()
 
-		//var $script = $comp('script')
-		//stuck here, how to execute it
+		var myssrcomp = new Myssrcomp()
+		//copy attributes
+		var comptag = $page('my-ssrcomp').get(0)
+		for (var name in comptag.attribs) { //cheerio
+			myssrcomp.setAttribute(name, comptag.attribs[name])
+		}
 
-		console.log('loaded component js on the serverside')
+		//skatejs/ssr, 
+		//https://medium.com/@treshugart/%C3%A5server-side-rendering-web-components-e5df705f3f48
+		render(myssrcomp, {rehydrate: false}).then(function(renderResult){
 
-		render(myssrcomp, {debug: true, rehydrate: false}).then(function(value){
-			$ssrcomp = cheerio.load(value)
+			$ssrcomp = cheerio.load(renderResult)
 			var styleId = $ssrcomp('style').first().attr('id')
 			$ssrcomp('my-ssrcomp').attr('data-style-id', styleId)
 			value = $ssrcomp.html()
-
-			console.log('ssr content'+value)
 			$page('my-ssrcomp').replaceWith(value)
-			//html = html.replace('<my-ssrcomp title="MINE"></my-ssrcomp>', value)
-
 			res.status(200).send( $page.html() ).end()
 		})
 
--- ssr-topseed-io/server/ssrComp/Myssrcomp.js
+++ ssr-topseed-io/server/ssrComp/Myssrcomp.js
@@ -4,29 +4,24 @@ class MySsrComp extends HTMLElement {
 
 	constructor(){
 		super()
-		console.log('Myssrcomp constructor')
+		console.log('============Myssrcomp constructor')
 	}
 
 	connectedCallback(){
-		console.log('----Myssrcomp connectedCallback')
-		//this.innerHTML = document.getElementById('xmy-ssrcomp').innerHTML
+		console.log('============Myssrcomp connectedCallback')
 
-		console.log('title attr:'+this.getAttribute('title')+'*')
-		
-		//this.innerHTML = $('#xmy-ssrcomp').html()
 		//document.getElementById('myssrcomp-title').innerHTML = this.getAttribute('title')||document.getElementById('myssrcomp-title').innerHTML
 		$('#myssrcomp-title').text(this.getAttribute('title')||$('#myssrcomp-title').text())
-		//$('#myssrcomp-title').text(this.getTitle()||$('#myssrcomp-title').text())
 
 		if (!this.shadowRoot) {
 			this.attachShadow({ mode: "open" }).innerHTML = $('#xmy-ssrcomp').html()
 		}
-		
 	}
 }
 
 customElements.define(MySsrComp.is, MySsrComp);
 
-module.exports = new MySsrComp()
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+	module.exports = MySsrComp
 	
 	
